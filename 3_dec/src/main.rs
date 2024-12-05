3289bf1757f7e267808897ca72069df7a5f929fe
Christer Steinfinsbø <csteinfinsbo@gmail.com>
2024-12-05 18:00:44 +0100

    3.dec #2

--- 3_dec/src/main.rs
+++ 3_dec/src/main.rs
@@ -14,15 +14,20 @@ fn read_file_contents(filename: &str) -> Result<String> {
 
 fn main() {
     let contents = read_file_contents("input.txt").unwrap();
-    let re = Regex::new(r"mul\((?<left>[0-9]+(\.?[0-9])*),(?<right>[0-9]+(\.?[0-9])*)\)").unwrap();
-    
+    let re = Regex::new(r"(?ms)(?:\A|do\(\))(?:.*?)(?:don't\(\)|\z)").unwrap();
+
     let sum: f64 = re.captures_iter(&contents)
         .map(|cap| {
-            let left = cap["left"].parse::<f64>().unwrap();
-            let right = cap["right"].parse::<f64>().unwrap();
-            left * right
-        })
-        .sum();
+            let re = Regex::new(r"(?ms)mul\((?P<left>[0-9]+),(?P<right>[0-9]+)\)").unwrap();    
+            let inner_cap = cap.get(0).unwrap().as_str();
+            re.captures_iter(inner_cap)
+                .map(|cap| {
+                    let left = cap["left"].parse::<f64>().unwrap();
+                    let right = cap["right"].parse::<f64>().unwrap();
+                    left * right
+                })
+                .sum::<f64>()      
+        }).sum();
 
     println!("Sum: {}", sum);
 }
