e5905ca3b3d2ccda3d51e02327000ad58ecd7c3f
Christer Steinfinsbø <csteinfinsbo@gmail.com>
2024-12-05 17:45:48 +0100

    1.dec #2

--- 1_dec/src/main.rs
+++ 1_dec/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::{HashMap, HashSet}, fs};
 
 
 fn main() {
@@ -11,9 +11,35 @@ fn main() {
     left.sort();
     right.sort();
 
+    let sim_count = compute_similarity_count(&left, &right);
+
     let result: i32 = compute_diff(left, right);
 
-    println!("{}", result);
+
+    println!("{}", sim_count);
+}
+
+fn compute_similarity_count(left: &Vec<i32>, right: &Vec<i32>) -> i32{
+    let right_map = count_occurrences(right);
+
+    let mut similarity_count = 0;
+    for l in left {
+        let count = right_map.get(l).unwrap_or(&0);
+        similarity_count += (l * count);
+    }
+
+    similarity_count
+}
+
+fn count_occurrences(list: &Vec<i32>) -> HashMap<i32, i32> {
+    let mut count_map: HashMap<i32, i32> = HashMap::new();
+
+    for val in list.iter() {
+        let count = count_map.entry(*val).or_insert(0);
+        *count += 1;
+    }
+
+    count_map
 }
 
 trait Joinable {
@@ -83,4 +109,23 @@ mod tests {
         assert_eq!(result, result);
     }
 
+    #[test]
+    fn test_count_occurences() {
+        let right = vec![1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4];
+        let result = count_occurrences(&right);
+
+        assert_eq!(result.get(&1), Some(&3));
+        assert_eq!(result.get(&2), Some(&3));
+        assert_eq!(result.get(&3), Some(&3));
+        assert_eq!(result.get(&4), Some(&3));
+    }
+
+    #[test]
+    fn test_compute_similarity_count() {
+        let left = vec![3 ,4 ,2 ,1 ,3 ,3];
+        let right = vec![4, 3, 5, 3, 9, 3];
+        let result = compute_similarity_count(&left, &right);
+
+        assert_eq!(result, 31);
+    }
 }
